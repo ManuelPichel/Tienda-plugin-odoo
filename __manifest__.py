ddef19d38ef3e5478d7e4e5a77631f75b8adf501
ManuelPichel <manupjr6@gmail.com>
2019-02-08 18:00:27 +0100

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Tienda',
+    'description': 'Tienda',
+    'summary' : 'Plugin para ventas',
+    'author': 'Manuel Pichel',
+    'depends': ['mail'],
+    'application': True,
+    'data': ['views/proveedores.xml', 'views/clientes.xml', 'views/articulos.xml', 'views/ventas.xml']
+}
